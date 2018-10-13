85bf89bce57248a421f6e472fd921f2e10d30289
Hugo <hugovk@users.noreply.github.com>
2018-10-13 15:31:36 +0300

    Add helper script

---
+++ do-charts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+python3 jsons2csv.py --chart --no-show -i "data/201*"
+python3 jsons2csv.py --chart --no-show -i "data/django*"
+python3 jsons2csv.py --chart --no-show -i "data/numpy*"
+python3 jsons2csv.py --chart --no-show -i "data/pillow*"
+python3 jsons2csv.py --chart --no-show -i "data/pylast*"
+open data/pip-install-*.png
