f5d55aba12f0f89c2fa8d07b45e16968a3e666f3
Hugo <hugovk@users.noreply.github.com>
2019-07-01 17:21:52 +0300

    Add 2019-06 data

---
+++ get-data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+python3 pypi-trends.py -f 2019-06
+python3 pypi-trends.py -f 2019-06 -p pillow
+python3 pypi-trends.py -f 2019-06 -p pytest
+python3 pypi-trends.py -f 2019-06 -p pip
+python3 pypi-trends.py -f 2019-06 -p pylast
+python3 pypi-trends.py -f 2019-06 -p coverage
+python3 pypi-trends.py -f 2019-06 -p numpy
+python3 pypi-trends.py -f 2019-06 -p django
+python3 pypi-trends.py -f 2019-06 -p matplotlib
+python3 pypi-trends.py -f 2019-06 -p pylint
+python3 pypi-trends.py -f 2019-06 -p flake8
+python3 pypi-trends.py -f 2019-06 -p six
+python3 pypi-trends.py -f 2019-06 -p tensorflow
+
+# pep8
+# pycodestyle
