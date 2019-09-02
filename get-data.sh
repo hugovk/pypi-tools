83215913199f98700be9756246b097baa5c4f393
Hugo <hugovk@users.noreply.github.com>
2019-09-02 12:09:53 +0300

    Add 2019-08 data

--- get-data.sh
+++ get-data.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
-python3 pypi-trends.py -f 2019-07
-python3 pypi-trends.py -f 2019-07 -p pillow
-python3 pypi-trends.py -f 2019-07 -p pytest
-python3 pypi-trends.py -f 2019-07 -p pip
-python3 pypi-trends.py -f 2019-07 -p pylast
-python3 pypi-trends.py -f 2019-07 -p coverage
-python3 pypi-trends.py -f 2019-07 -p numpy
-python3 pypi-trends.py -f 2019-07 -p django
-python3 pypi-trends.py -f 2019-07 -p matplotlib
-python3 pypi-trends.py -f 2019-07 -p pylint
-python3 pypi-trends.py -f 2019-07 -p flake8
-python3 pypi-trends.py -f 2019-07 -p six
-python3 pypi-trends.py -f 2019-07 -p tensorflow
+
+FROM="2019-08"
+
+python3 pypi-trends.py -f $FROM
+python3 pypi-trends.py -f $FROM -p pillow
+python3 pypi-trends.py -f $FROM -p pytest
+python3 pypi-trends.py -f $FROM -p pip
+python3 pypi-trends.py -f $FROM -p pylast
+python3 pypi-trends.py -f $FROM -p coverage
+python3 pypi-trends.py -f $FROM -p numpy
+python3 pypi-trends.py -f $FROM -p django
+python3 pypi-trends.py -f $FROM -p matplotlib
+python3 pypi-trends.py -f $FROM -p pylint
+python3 pypi-trends.py -f $FROM -p flake8
+python3 pypi-trends.py -f $FROM -p six
+python3 pypi-trends.py -f $FROM -p tensorflow
 
 # pep8
 # pycodestyle
