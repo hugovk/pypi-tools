6321f29e4c3ae9338e24f8422e7dd23893baa249
Hugo <hugovk@users.noreply.github.com>
2020-01-08 17:14:56 +0200

    Re-add December data, a day or two was missing

--- get-data.sh
+++ get-data.sh
@@ -1,24 +1,25 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 FROM="2019-12"
 TO=$FROM
 
 python3 pypi-trends.py -f $FROM -t $TO
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p coverage
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p django
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p flake8
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p matplotlib
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p numpy
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pandas
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pillow
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pytest
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pip
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pylast
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p coverage
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p numpy
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p django
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p matplotlib
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pylint
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p flake8
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p six
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p tensorflow
-python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pandas
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p pytest
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p requests
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p scipy
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p six
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p tensorflow
+
 python3 pypi-trends.py -p scipy
 
 # pep8
