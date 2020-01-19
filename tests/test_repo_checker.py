1ca87d0f36b5add81ff9dffca784c13fe9afb673
Hugo <hugovk@users.noreply.github.com>
2020-01-19 23:03:34 +0200

    Add tool to clone each top repo, then run a test command on each

---
+++ tests/test_repo_checker.py
@@ -0,0 +1,25 @@
+import pytest
+
+import repo_checker
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        "http://github.com/octocat/Hello-World",
+        "http://github.com/octocat/Hello-World/",
+        "https://github.com/octocat/Hello-World",
+        "https://github.com/octocat/Hello-World.git",
+        "https://github.com/octocat/Hello-World.git",
+        "https://github.com/octocat/Hello-World/",
+    ],
+)
+def test_repo_url_dir_name(test_input):
+    # Arrange
+    repo_url = test_input
+
+    # Act
+    dir_name = repo_checker.repo_url_dir_name(repo_url)
+
+    # Assert
+    assert dir_name == "Hello-World"
