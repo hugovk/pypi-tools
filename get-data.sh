554faab134773105c46943923295c0e91e7457bc
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-05-01 21:47:07 +0300

    Add 2021-04 data

--- get-data.sh
+++ get-data.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-FROM="2021-03"
+FROM="2021-04"
 TO=$FROM
 
 # application_limits=["5 per second", "30 per minute"]
 
 python3 pypi-trends.py -f $FROM -t $TO
+python3 pypi-trends.py --pypistats -f $FROM -t $TO -p black
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p coverage
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p django
 python3 pypi-trends.py --pypistats -f $FROM -t $TO -p flake8
@@ -36,7 +37,6 @@ python3 pypi-trends.py --pypistats -f $FROM -t $TO -p virtualenv
 
 # Just the "free" data for these for now
 python3 pypi-trends.py --pypistats -p attrs
-python3 pypi-trends.py --pypistats -p black
 python3 pypi-trends.py --pypistats -p colorama
 python3 pypi-trends.py --pypistats -p cryptography
 python3 pypi-trends.py --pypistats -p httpx
