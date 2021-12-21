34659c1ec407501828bd75b157144351521f5559
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-12-21 12:33:28 +0200

    Add handling for string fields like requires_python

--- project_urls.py
+++ project_urls.py
@@ -1,9 +1,9 @@
 """
-Read in packages from top-pypi-packages.json and fetch their project_urls metadata.
+Read in packages from top-pypi-packages.json and fetch their metadata for a given field.
 
 (Metadata is cached by source_finder.py in CACHE_DIR.)
 
-Return a count of the most common keys.
+Return a count of the most common values (for strings) or keys (for dicts).
 
 Start by:
 
@@ -11,18 +11,50 @@ Start by:
 wget https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json -O \
     data/top-pypi-packages.json
 
-# Check the first 10 packages in data/top-pypi-packages.json
-python3 project_urls.py --number 10
+# Show requires_python for the first 10 packages in data/top-pypi-packages.json
+python3 project_urls.py --number 10 --field requires_python
+Load data/top-pypi-packages.json...
+Find requires_python...
+100%|█████████████████████████████████████████████| 10/10 [00:00<00:00, 104.96project/s]
+
+| requires_python                                             | Count |
+|:------------------------------------------------------------|------:|
+| >= 3.6                                                      |     4 |
+| !=3.0.*,!=3.1.*,!=3.2.*,>=2.7                               |     1 |
+| >=2.7, !=3.0.*, !=3.1.*, !=3.2.*                            |     1 |
+| >=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.* |     1 |
+| >=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4      |     1 |
+| >=3.6                                                       |     1 |
+| >=3.7                                                       |     1 |
+
+Projects with requires_python: 10/10
+
+# Show project_urls for the first 10 packages and also the Source URLs
+python3 project_urls.py --number 10 --key Source
 Load data/top-pypi-packages.json...
 Find project_urls...
-100%|█████████████████████████████████████████████| 10/10 [00:00<00:00, 167.91project/s]
-Counter({'Homepage': 10,
-         'Documentation': 4,
-         'Source': 2,
-         'Code': 1,
-         'Issue tracker': 1})
+100%|█████████████████████████████████████████████| 10/10 [00:00<00:00, 154.35project/s]
 
-Number with project_urls: 10/10
+| project_urls  | Count |
+|:--------------|------:|
+| Homepage      |    10 |
+| Documentation |     6 |
+| Source        |     3 |
+| Bug Tracker   |     1 |
+| CI            |     1 |
+| Code          |     1 |
+| Download      |     1 |
+| Issue tracker |     1 |
+| Mailing lists |     1 |
+| Source Code   |     1 |
+
+Projects with project_urls: 10/10
+
+| Project         | Source URL                           |
+|:----------------|:-------------------------------------|
+| boto3           | https://github.com/boto/boto3        |
+| python-dateutil | https://github.com/dateutil/dateutil |
+| requests        | https://github.com/psf/requests      |
 """
 import argparse
 import collections
@@ -60,7 +92,9 @@ def main():
         "-n", "--number", type=int, default=100, help="Max number to fetch"
     )
     parser.add_argument("-f", "--field", default="project_urls", help="Show this field")
-    parser.add_argument("-k", "--key", help="Also show fields with this key")
+    parser.add_argument(
+        "-k", "--key", help="For dict fields, also show the values for this key"
+    )
     args = parser.parse_args()
 
     # Load from top-pypi-packages.json
@@ -70,27 +104,38 @@ def main():
 
     print(f"Find {args.field}...")
 
+    fields = []
     all_keys = []
     selected_urls = []
     count = 0
     for package in tqdm(packages_todo, unit="project"):
         field = get_field(args.field, package["name"])
         if field:
-            all_keys.extend(field.keys())
-            count += 1
-            if args.key:
-                try:
-                    selected_urls.append((package["name"], field[args.key]))
-                except KeyError:
-                    pass
+            fields.append(field)
+            if isinstance(field, dict):
+                all_keys.extend(field.keys())
+                count += 1
+                if args.key:
+                    try:
+                        selected_urls.append((package["name"], field[args.key]))
+                    except KeyError:
+                        pass
 
-    # Counter will sort by most common, but let's sort alphabetically
-    # for those with the same count
-    all_keys = sorted(all_keys)
-    most_common = collections.Counter(all_keys).most_common()
+    # For strings, print table of most common
+    if isinstance(fields[0], str):
+        fields = sorted(fields)
+        most_common = collections.Counter(fields).most_common()
+        count = len(fields)
+
+    # For dicts, print table of most common keys
+    elif isinstance(fields[0], dict):
+        # Counter will sort by most common, but let's sort alphabetically
+        # for those with the same count
+        all_keys = sorted(all_keys)
+        most_common = collections.Counter(all_keys).most_common()
 
     table = PrettyTable()
-    table.field_names = ["Project URL", "Count"]
+    table.field_names = [args.field, "Count"]
     table.align = "l"
     table.align["Count"] = "r"
     table.set_style(MARKDOWN)
